89dd6e3f5a4909740aeeef9c976db83a0d6d65c2
kongpf <kongpf@zenmen.com>
2023-06-01 14:19:36 +0800

    update oauth code

--- app/src/main/java/com/kongpf8848/dmail/login/oauth/hotmail/HotmailModule.kt
+++ app/src/main/java/com/kongpf8848/dmail/login/oauth/hotmail/HotmailModule.kt
@@ -1,23 +1,23 @@
-package com.kongpf8848.dmail.login.oauth.hotmail;
+package com.kongpf8848.dmail.login.oauth.hotmail
 
-import dagger.Module;
-import dagger.Provides;
-import okhttp3.OkHttpClient;
-import retrofit2.Retrofit;
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
-import retrofit2.converter.gson.GsonConverterFactory;
+import dagger.Module
+import dagger.Provides
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 
 @Module
-public class HotmailModule {
+class HotmailModule {
 
     @Provides
-    public HotmailApi provideHotmailApi() {
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://apis.live.net/")
-                .client(new OkHttpClient())
-                .addConverterFactory(GsonConverterFactory.create())
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                .build();
-        return retrofit.create(HotmailApi.class);
+    fun provideHotmailApi(): HotmailApi {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://apis.live.net/")
+            .client(OkHttpClient())
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build()
+        return retrofit.create(HotmailApi::class.java)
     }
 }
--- app/src/main/java/com/kongpf8848/dmail/login/oauth/yahoo/YahooModule.kt
+++ app/src/main/java/com/kongpf8848/dmail/login/oauth/yahoo/YahooModule.kt
@@ -1,23 +1,23 @@
-package com.kongpf8848.dmail.login.oauth.yahoo;
+package com.kongpf8848.dmail.login.oauth.yahoo
 
-import dagger.Module;
-import dagger.Provides;
-import okhttp3.OkHttpClient;
-import retrofit2.Retrofit;
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
-import retrofit2.converter.gson.GsonConverterFactory;
+import dagger.Module
+import dagger.Provides
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 
 @Module
-public class YahooModule {
+class YahooModule {
 
     @Provides
-    public YahooApi provideYahooApi() {
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://api.login.yahoo.com/")
-                .client(new OkHttpClient())
-                .addConverterFactory(GsonConverterFactory.create())
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                .build();
-        return retrofit.create(YahooApi.class);
+    fun provideYahooApi(): YahooApi {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.login.yahoo.com/")
+            .client(OkHttpClient())
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build()
+        return retrofit.create(YahooApi::class.java)
     }
 }
